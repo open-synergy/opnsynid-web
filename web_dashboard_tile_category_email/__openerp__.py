a322d2d1e59311b6414c7614d737dd9e0fd25ad9
Andhitia Rama <andhitia.r@gmail.com>
2019-12-25 09:22:40 +0700

    [8.0.1.0.0] web_dashboard_tile_category_email

---
+++ web_dashboard_tile_category_email/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Email Dashboard Tile Value",
+    "version": "8.0.1.0.0",
+    "category": "web",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "email_template",
+        "web_dashboard_tile_category",
+    ],
+    "data": [
+        "views/tile_category_views.xml",
+    ],
+}
