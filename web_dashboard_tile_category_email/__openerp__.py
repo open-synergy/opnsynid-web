ec49625e2b541fec7001fa48324904da36871d33
Andhitia Rama <andhitia.r@gmail.com>
2019-12-25 17:27:21 +0700

    [8.0.2.0.0] web_dashboard_tile_category_email
    * inherit tile.category from mail.thread

--- web_dashboard_tile_category_email/__openerp__.py
+++ web_dashboard_tile_category_email/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Email Dashboard Tile Value",
-    "version": "8.0.1.0.0",
+    "version": "8.0.2.0.0",
     "category": "web",
     "website": "https://opensynergy-indonesia.com/",
     "author": "OpenSynergy Indonesia",
